e843a1ce9370118fe9eeffe1f6cb0447df576d88
Prakhya <saiprakhya.mylavaram@sjsu.edu>
2023-12-05 11:36:45 -0800

    final-changes

---
+++ src/app/components/sidebar/sidebar.component.ts
@@ -0,0 +1,68 @@
+import { Component, OnInit } from '@angular/core';
+
+declare interface RouteInfo {
+    path: string;
+    title: string;
+    icon: string;
+    class: string;
+    submenu?: RouteInfo[]; 
+}
+export const ROUTES: RouteInfo[] = [
+    { path: '/dashboard', title: 'Dashboard',  icon: 'design_app', class: '' },
+    { path: '/maps', title: 'Maps',  icon:'location_map-big', class: '' },
+    { 
+      path: '/table-list', 
+      title: 'Station Management',  
+      icon:'location_pin', 
+      class: '', 
+      submenu: [
+          { path: '/add-stations', title: ' Add Stations', icon: 'ui-1_simple-add', class: '' },
+          { path: '/dashboard', title: 'Maintanace', icon: 'ui-1_settings-gear-63', class: '' },
+      
+      ] 
+    },
+    { path: '/icons', title: 'TRANSACTIONS',  icon:'shopping_credit-card', class: '' },
+  
+
+];
+
+@Component({
+  selector: 'app-sidebar',
+  templateUrl: './sidebar.component.html',
+  styleUrls: ['./sidebar.component.css']
+})
+export class SidebarComponent implements OnInit {
+  menuItems!: any[];
+  activeMenuItem: string = '';
+  activeSubmenu: string = '';
+  showSubmenu: boolean = false;
+
+setActive(path: string) {
+    this.activeMenuItem = path;
+}
+toggleSubmenu(submenuTitle: string): void {
+  this.activeSubmenu = this.activeSubmenu === submenuTitle ? '' : submenuTitle;
+}
+
+isActive(path: string) {
+    return this.activeMenuItem === path;
+}
+setSubmenuActive(submenu: string) {
+  this.activeSubmenu = submenu === this.activeSubmenu ? '' : submenu;
+}
+isSubmenuActive(submenu: string) {
+  return this.activeSubmenu === submenu;
+}
+
+  constructor() { }
+
+  ngOnInit() {
+    this.menuItems = ROUTES.filter(menuItem => menuItem);
+  }
+  isMobileMenu() {
+      if ( window.innerWidth > 991) {
+          return false;
+      }
+      return true;
+  };
+}
---
+++ src/app/layouts/admin-layout/admin-layout.routing.ts
@@ -0,0 +1,16 @@
+import { Routes } from '@angular/router';
+
+import { DashboardComponent } from '../../dashboard/dashboard.component';
+import { TableListComponent } from '../../addstations/table-list.component';
+import { IconsComponent } from '../../transactions/icons.component';
+import { MapsComponent } from '../../maps/maps.component';
+import { AddStationComponent } from '../../add-station/add-station.component';
+
+export const AdminLayoutRoutes: Routes = [
+    { path: 'dashboard',      component: DashboardComponent },
+    { path: 'table-list',     component: TableListComponent },
+    { path: 'maps',           component: MapsComponent },
+    { path: 'icons',          component: IconsComponent},
+    { path: 'add-stations',    component: AddStationComponent}
+    
+];
